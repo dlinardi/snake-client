6b0ce330478ae1f690ee32b73d8bfe60aa17e437
dlinardi <dlinardi98@gmail.com>
2020-11-05 01:57:22 +0000

    implemented handling of user input for movement

--- input.js
+++ input.js
@@ -1,17 +1,37 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
+
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
   stdin.on('data', handleUserInput);
+  // stdin.on('data', key => {
+  //   if (key === 'w') console.log('up');
+  // });
 
   return stdin;
-}
+};
 
-const handleUserInput = () => {
+const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
